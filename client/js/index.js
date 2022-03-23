64dfb19e8bb5299f4a19795ef07485b745195996
Henry <uce.henrys@gmail.com>
2022-03-23 12:24:11 +0000

    to post and view data from the database

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,7 @@
 const myForm = document.querySelector("#post");
+const fill = document.querySelector("#toBeFilled");
 
-myForm.addEventListener("submit", submitForm)
+myForm.addEventListener("submit", submitForm);
 
 function submitForm(e) {
     e.preventDefault();
@@ -22,9 +23,22 @@ function submitForm(e) {
     }
 
     fetch(url, options)
-        .then(res => res.json())
-        .catch(console.warn)
-};
+    .then(res => res.json())
+    .catch(console.warn);
+    
+    
+    fill.innerHTML = "";
+    fetch(url) 
+    .then(res => res.json())
+    .then(res => {
+            res.forEach(element => {
+                let newList = document.createElement('li');
+                newList.textContent = `Title: ${element.title} + Author: ${element.author} + Content: ${element.content}`;
+                fill.append(newList);
+            });
+    })
+    .catch(console.warn)
+}
 
 async function getPost(title, time, id){
     try {
@@ -35,35 +49,3 @@ async function getPost(title, time, id){
         console.warn(err);
     }
 }
-
-
-
-// async function submitForm(e) {
-//     e.preventDefault();
-//     try {
-//         const postData = {
-//             title: e.target.title.value,
-//             author: e.target.author.value,
-//             content: e.target.content.value 
-//         }
-
-//         console.log(postData);
-
-//         const options = {
-//             method: 'POST',
-//             body: JSON.stringify(postData),
-//             headers: {
-//                 "Content-Type" : "application/json"
-//             }
-//         }
-
-//         const response = await fetch(url, options);
-//         const data = await response.json();
-//         return data;
-        
-//     } catch(err) {
-//         console.warn(err);
-//     }
-// };
-
-
