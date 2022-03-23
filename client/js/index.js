9ea4e658269d666d55463ae1f33ab85827defe7c
Henry <uce.henrys@gmail.com>
2022-03-23 11:47:51 +0000

    full commit

--- client/js/index.js
+++ client/js/index.js
@@ -26,6 +26,17 @@ function submitForm(e) {
         .catch(console.warn)
 };
 
+async function getPost(title, time, id){
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        return data;
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+
 
 // async function submitForm(e) {
 //     e.preventDefault();
