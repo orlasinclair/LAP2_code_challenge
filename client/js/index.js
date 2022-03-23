908115fced41b17b9a7dd80f6fb714c17ac27a3b
Henry <uce.henrys@gmail.com>
2022-03-23 11:46:46 +0000

    created basic frontside with post request - stub code

---
+++ client/js/index.js
@@ -0,0 +1,58 @@
+const myForm = document.querySelector("#post");
+
+myForm.addEventListener("submit", submitForm)
+
+function submitForm(e) {
+    e.preventDefault();
+
+    const postData = {
+        title: e.target.title.value,
+        author: e.target.author.value,
+        content: e.target.content.value 
+    }
+
+    console.log(postData);
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    }
+
+    fetch(url, options)
+        .then(res => res.json())
+        .catch(console.warn)
+};
+
+
+// async function submitForm(e) {
+//     e.preventDefault();
+//     try {
+//         const postData = {
+//             title: e.target.title.value,
+//             author: e.target.author.value,
+//             content: e.target.content.value 
+//         }
+
+//         console.log(postData);
+
+//         const options = {
+//             method: 'POST',
+//             body: JSON.stringify(postData),
+//             headers: {
+//                 "Content-Type" : "application/json"
+//             }
+//         }
+
+//         const response = await fetch(url, options);
+//         const data = await response.json();
+//         return data;
+        
+//     } catch(err) {
+//         console.warn(err);
+//     }
+// };
+
+
