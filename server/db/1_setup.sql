a23f05cf200e5a00d0b4f6adb9778c0fea27d85d
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-03-23 16:20:59 +0000

    delete files

--- server/db/1_setup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    name VARCHAR,
-    body VARCHAR NOT NULL,
-    posting_date VARCHAR
-);
--- server/db/2_seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO posts (title, name, body, posting_date)
-VALUES
-    ('1st Post', 'anonymous', 'testing testing', '');
