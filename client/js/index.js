cea5ff8308cf2ee1608894c7d2c3f55a746b1586
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-03-24 11:03:16 +0000

    changes

--- client/js/index.js
+++ client/js/index.js
@@ -3,76 +3,92 @@ const fill = document.querySelector("#toBeFilled");
 
 myForm.addEventListener("submit", submitForm);
 
-function submitForm(e) {
+async function submitForm(e) {
     e.preventDefault();
+    
+        var currentdate = new Date();
+        let author = e.target.author.value;
+        var datetime = `${currentdate.getDate()}/${(currentdate.getMonth() + 1)}/${currentdate.getFullYear()}`;
+        // " - " +
+        // currentdate.getHours() +
+        // ":" +
+        // currentdate.getMinutes() +
+        // ":" +
+        // currentdate.getSeconds();
 
-    var currentdate = new Date();
-    let author = e.target.author.value;
-    var datetime = `${currentdate.getDate()}/${(currentdate.getMonth() + 1)}/${currentdate.getFullYear()}`;
-    // " - " +
-    // currentdate.getHours() +
-    // ":" +
-    // currentdate.getMinutes() +
-    // ":" +
-    // currentdate.getSeconds();
-
-    const postData = {
-        title: e.target.title.value,
-        name: author,
-        body: e.target.body.value,
-        posting_date: datetime
-    }
-
-    console.log(postData);
-
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: {
-            "Content-Type": "application/json"
+        const postData = {
+            title: e.target.title.value,
+            name: author,
+            body: e.target.body.value,
+            posting_date: datetime
         }
-    }
 
-    fetch("http://localhost:3000/posts", options)
-        .then(res => res.json())
-        .catch(console.warn);
-    changeHash();
+        console.log(postData);
+        console.log('update')
+    try{
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const response = await fetch("http://localhost:3000/posts", options);
+        let data = await response.json;
+        console.log(data)
+        console.log(data.id)
+
+        if(data.err){
+            throw Error(data.err)
+        }
+        else(
+            window.location.hash = `#${data.id}`
+        );
+        // changeHash();
+    } catch (err) {
+        console.warn(err)
+    }
 }
 // Extracting the hash from the entire URL
 // var hash = window.location.hash.substring(1);
 
 
-function navigate(id) {
-    var current = window.location.href;
-    window.location.href = current.replace(/#(.*)$/, '') + '#' + id;
-}
+// function navigate(id) {
+//     var current = window.location.href;
+//     window.location.href = current.replace(/#(.*)$/, '') + '#' + id;
+// }
 
 
 
-function changeHash() {
+// async function changeHash() {
 
-    fetch(`http://localhost:3000/posts/`)
-        .then(res => res.json())
-        .then(res => {
-            const id = res.posts.length + 1;
-            navigate(id);
-        })
-        .catch(console.warn)
+//     fetch(`http://localhost:3000/posts/`)
+//         .then(res => res.json())
+//         .then(res => {
+//             const id = res.posts.id;
+//             navigate(id);
+//         })
+//         .catch(console.warn)
 
-    var hash = window.location.href.split('#')[1] || '';
-    console.log("this is hash: " + hash);
+//     var hash = window.location.href.split('#')[1] || '';
+//     console.log("this is hash: " + hash);
 
-    fill.innerHTML = "";
-    fetch(`http://localhost:3000/posts/${hash}`)
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            console.log("This is the title: " + res.postid.title);
-            let newList = document.createElement('li');
-            newList.textContent = `Title: ${res.postid.title} + Author: ${res.postid.name} + Current Date: ${res.postid.posting_date}
-                                            + Content: ${res.postid.body}`;
-            fill.append(newList);
+// async function getPostById(id){
 
-        })
-        .catch(console.warn)
-}
+//     fill.innerHTML = "";
+//     try{
+
+//           const response = await fetch(`http://localhost:3000/posts/${id}`)
+//           let data = await response.json;
+//           console.log(data)
+//           return data
+//         }
+//     catch(err){console.warn(err)}
+// }
+
+
+// let newList = document.createElement('li');
+//             newList.textContent = `Title: ${res.postid.title} + Author: ${res.postid.name} + Current Date: ${res.postid.posting_date}
+//                                             + Content: ${res.postid.body}`;
+//             fill.append(newList);
